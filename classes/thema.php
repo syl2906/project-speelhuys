c60658567d5089fc1f20102763a8fb26152d89e5
Goennigoegoe <tyrnan.degraaf@gmail.com>
2026-09-02 12:02:36 +0200

    thema class

---
+++ classes/thema.php
@@ -0,0 +1,58 @@
+<?php
+
+class Thema
+{
+    public int $ID;
+    public string $naam;
+
+    public function initializeer($inID): bool
+    {
+        require_once "database.php";
+        $database = new Database();
+        $database->start();
+
+        $veiligID = mysqli_real_escape_string($database->conn, $id);
+        $query = "SELECT * FROM themes WHERE theme_id = " . $veiligID;
+        $resultaat = $database->conn->query($query);
+
+        if($resultaat->num_rows > 0)
+        {
+            $rij = $resultaat->fetch_assoc();
+            $this->ID = $rij["theme_id"];
+            $this->naam = $rij["theme_name"];
+            $database->close();
+            return true;
+        }
+
+        $database->close();
+        return false;
+    }
+
+    public static function vindAlleThemas()
+    {
+        require_once "database.php";
+        $database = new Database();
+        $database->start();
+
+        $query = "SELECT * FROM themes";
+        $resultaat = $database->conn->query($query);
+        
+        $themas = [];
+        if($resultaat->num_rows > 0)
+        {
+            while($rij = $resultaat->fetch_assoc())
+            {
+                $thema = new Thema();
+                $thema->ID = $rij["theme_id"];
+                $thema->naam = $rij["theme_name"];
+
+                $themas[] = $thema;
+            }
+        }
+
+        $database->close();
+        return $themas;
+    }
+}
+
+?>
